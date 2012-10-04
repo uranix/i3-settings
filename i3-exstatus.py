10ebacf298d4b5e647d5d97ff025c174f60ad4e0
Ivan Tsybulin <tsybulinhome@gmail.com>
2012-10-04 21:12:25 +0400

    Changed bindings. Add more colors to i3-exstatus

--- i3-exstatus.py
+++ i3-exstatus.py
@@ -5,49 +5,62 @@ import sys
 import json
 import socket
 
+bat_color = {
+    'BAT' : '#ff0000',
+    'CHR' : '#ffff00',
+    'FUL' : '#ffffff',
+    'NCH' : '#ffffff',
+}
+
 def query_icedove():
-	try:
-		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		s.connect(('localhost', 3411))
-		reply = s.recv(8192)
-		s.close()
-		obj = json.loads(reply)
-		cnt = 0;
-		for accoutns in obj:
-			cnt = cnt + accoutns['unread']
-		if cnt == 0:
-			return ['No new mail', '#ffffff']
-		else:
-			return ['Have new mail', '#ffff00']
-	except Exception as e:
-		return ['Mailer offline', '#ff0000']
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect(('localhost', 3411))
+        reply = s.recv(8192)
+        s.close()
+        obj = json.loads(reply)
+        cnt = 0;
+        for accoutns in obj:
+            cnt = cnt + accoutns['unread']
+        if cnt == 0:
+            return ['No mail', '#ffffff']
+        else:
+            return ['New mail', '#ffff00']
+    except Exception as e:
+        return ['Mailer offline', '#ff0000']
 
 def print_line(msg):
-	sys.stdout.write(msg + '\n')
-	sys.stdout.flush()
+    sys.stdout.write(msg + '\n')
+    sys.stdout.flush()
 
 def read_line():
-	try:
-		line = sys.stdin.readline().strip();
-		if not line:
-			sys.exit(3);
-		return line
-	except KeyboardInterrupt:
-		sys.exit()
+    try:
+        line = sys.stdin.readline().strip();
+        if not line:
+            sys.exit(3);
+        return line
+    except KeyboardInterrupt:
+        sys.exit()
 
 if __name__ == '__main__':
-	# {"version" : 1}
-	print_line(read_line())
-	# [
-	print_line(read_line())
+    # {"version" : 1}
+    print_line(read_line())
+    # [
+    print_line(read_line())
 
-	while True:
-		line, prefix = read_line(), ''
-		if line.startswith(','):
-			line, prefix = line[1:], ','
+    while True:
+        line, prefix = read_line(), ''
+        if line.startswith(','):
+            line, prefix = line[1:], ','
 
-		j = json.loads(line)
-		mail = query_icedove()
-		j.insert(0, {'full_text' : mail[0], 'color' : mail[1], 'name' : 'icedove'})
-		
-		print_line(prefix + json.dumps(j));
+        j = json.loads(line)
+        mail = query_icedove()
+        j.insert(0, {'full_text' : mail[0], 'color' : mail[1], 'name' : 'icedove'})
+        for x in j:
+            if x['name'] == 'battery':
+                sig = x['full_text'][0:3]
+                x['color'] = bat_color[sig]
+                if x['full_text'][-1:] == ' ':
+                    x['full_text'] = x['full_text'][:-1]
+
+        print_line(prefix + json.dumps(j))
